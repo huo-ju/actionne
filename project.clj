950bbbd0562bfed6e3edad8118565142fb452f85
huoju <huoju@protonmail.com>
2019-04-25 02:40:33 -0400

    add http server, add twitter client

--- project.clj
+++ project.clj
@@ -3,11 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :main snsguardian.core
-  :aot [snsguardian.core]
+  :main  ^:skip-aot snsguardian.core
+  ;:aot [snsguardian.core]
   :profiles {:uberjar {:aot :all} }
-  :dependencies [[org.clojure/clojure "1.9.0"]
-    [instaparse "1.4.9"], 
+  :plugins [ [lein-ring "0.12.0"] [lein-cljfmt "0.6.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"],
+    [instaparse "1.4.9"] ,
+    [metosin/compojure-api "1.1.11"]
+    [ring/ring-jetty-adapter "1.6.3"]
     [com.cerner/clara-rules "0.18.0"]
+    [tea-time "1.0.1"]
+    [twttr "3.2.2"]
     ]
   :repl-options {:init-ns snsguardian.core})
--- src/snsguardian/core.clj
+++ src/snsguardian/core.clj
@@ -1,9 +1,19 @@
 (ns snsguardian.core
   (:gen-class)
   (:require [clojure.string :as string]
+            [clojure.tools.logging :as log]
             [instaparse.core :as insta]
             [clara.rules.accumulators :as acc]
             [clara.rules :refer :all]
+            [tea-time.core :as tt]
+            [compojure.api.sweet :refer [api routes]]
+            [compojure.api.exception :as ex]
+            [ring.util.http-response :refer :all]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [twttr.api :as api]
+            [twttr.auth :refer :all]
+            [snsguardian.approutes :refer [app-routes]]
+
   ))
 
 (defrecord Msgs [id target obj])
@@ -123,25 +133,44 @@
         (println action id))
     )
 )
-(defn -main [& args]
 
-    (let [parse-tree (parser example-rules)]
-        
-        (let [transformed (insta/transform transform-options parse-tree)]
-            (clojure.pprint/pprint transformed)
-            (let [[ver dslns]  transformed]
-                (let [session (-> (mk-session 'snsguardian.core transformed)
-                              (insert (->Msgs "msg1" "reply" {:like 10 :rt 12 :name "test"}))
-                              (insert (->Msgs "msg2" "reply" {:like 1 :rt 10 :name "111paaabbb"}))
-                              (insert (->Msgs "msg3" "reply" {:like 6 :rt 15 :name "111222"})) 
-                              (fire-rules))]
-                (println "====action")
-                (let [actionmsgs (query session get-actionmsgs)]
-                    (map (fn [msg] (doaction dslns msg)) actionmsgs)
-                )
-                )
-            )
+(def app (api (apply routes app-routes)))
+
+(def creds (map->UserCredentials {:consumer-key "", :consumer-secret ""}))
+;(env->UserCredentials)
+
+(defn -main [& args]
+    ;(let [parse-tree (parser example-rules)]
+    ;    
+    ;    (let [transformed (insta/transform transform-options parse-tree)]
+    ;        ;(clojure.pprint/pprint transformed)
+    ;        (let [[ver dslns]  transformed]
+    ;            (let [session (-> (mk-session 'snsguardian.core transformed)
+    ;                          (insert (->Msgs "msg1" "reply" {:like 10 :rt 12 :name "test"}))
+    ;                          (insert (->Msgs "msg2" "reply" {:like 1 :rt 10 :name "111paaabbb"}))
+    ;                          (insert (->Msgs "msg3" "reply" {:like 6 :rt 15 :name "111222"})) 
+    ;                          (fire-rules))]
+    ;            (println "====action")
+    ;            (let [actionmsgs (query session get-actionmsgs)]
+    ;                (println actionmsgs)
+    ;                (map (fn [msg] (doaction dslns msg)) actionmsgs)
+    ;            )
+    ;            )
+    ;        )
+    ;    )
+    ;)
+    (log/info "start...")
+    (let [tweets (api/statuses-user-timeline creds :params {:screen_name "virushuo"})]
+        (doseq [tweet tweets] 
+            (prn tweet)
+            (prn "====")
         )
+        
     )
+    ;(prn creds )
+    ;(tt/start!)
+    ;(def task (tt/every! 2 (bound-fn [] (log/info "hi."))))
+    ;(print "ok")
+    ;(run-jetty app {:port 3000})
 )
 
